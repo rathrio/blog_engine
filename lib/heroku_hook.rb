8661aabb23b89d40f59f4997591c8548f1a10983
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2013-09-07 17:06:18 +0200

    Basic caching

--- lib/heroku_hook.rb
+++ lib/heroku_hook.rb
@@ -1,18 +1,8 @@
 require 'sinatra/base'
 
 class HerokuHook < Sinatra::Base
-  def self.deployed
-    set(:last_deployed) { Time.now }
-  end
-
-  deployed
-
   before do
     cache_control :public, :must_revalidate
-    last_modified settings.last_deployed
-  end
-
-  post '/update' do
-    settings.deployed
+    last_modified Time.now
   end
 end
