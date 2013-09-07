59914c379224e6458b8dffc07736b6de879b037c
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2013-09-07 16:55:02 +0200

    Caching via last_modifier header

--- lib/heroku_hook.rb
+++ lib/heroku_hook.rb
@@ -1,7 +1,18 @@
 require 'sinatra/base'
 
 class HerokuHook < Sinatra::Base
+  def self.deployed
+    set(:last_deployed) { Time.now }
+  end
+
+  deployed
+
+  before do
+    cache_control :public, :must_revalidate
+    last_modified settings.last_deployed
+  end
+
   post '/update' do
-    raise 'i got to the heroku hook'
+    settings.deployed
   end
 end
