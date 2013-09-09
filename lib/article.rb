e30a734c68a7d77d50d0ae73ccb518fe129000d2
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2013-09-09 12:39:10 +0200

    Updated Articles. Added delimiter for notes.

--- lib/article.rb
+++ lib/article.rb
@@ -5,11 +5,17 @@ require 'yaml'
 class Article < OpenStruct
   def self.from_file(filename)
     meta, content       = File.read(filename).split("\n\n", 2)
+    content, misc       = split_misc_from_content content
     article             = new YAML.load(meta)
     article.date        = Time.parse article.date.to_s
     article.content     = content
+    article.misc        = misc
     article.slug        = File.basename(filename, '.md')
     article.author_slug = article.author.downcase # TODO: #underscore
     article
+  end
+
+  def self.split_misc_from_content(content)
+    content.split /\n\n\/\/(NOTES|NOTE|OUTLINE|MISC)\/\/\n\n/, 2
   end
 end
