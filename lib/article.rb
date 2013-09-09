86124ea06a7b53c86beda167cc1dbde48cc1d195
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2013-09-09 10:42:45 +0200

    Simplified article

--- lib/article.rb
+++ lib/article.rb
@@ -4,16 +4,12 @@ require 'yaml'
 
 class Article < OpenStruct
   def self.from_file(filename)
-    meta, content   = File.read(filename).split("\n\n", 2)
-    article         = new YAML.load(meta)
-    article.date    = Time.parse article.date.to_s
-    article.content = content
-    article.slug    = File.basename(filename, '.md')
+    meta, content       = File.read(filename).split("\n\n", 2)
+    article             = new YAML.load(meta)
+    article.date        = Time.parse article.date.to_s
+    article.content     = content
+    article.slug        = File.basename(filename, '.md')
+    article.author_slug = article.author.downcase # TODO: #underscore
     article
-  end
-
-  def author_slug
-    # TODO: #underscore
-    author.downcase
   end
 end
