d371c9eb63ae78a588a0e561eee6b3ffaf503fc2
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2013-09-15 12:09:20 +0200

    Updated Articles
    * Some style changes
    * Removed notes feature

--- lib/article.rb
+++ lib/article.rb
@@ -8,18 +8,12 @@ class Article < OpenStruct
 
   def self.from_file(filename)
     meta, content       = File.read(filename).split("\n\n", 2)
-    content, misc       = split_misc_from_content content
     article             = new YAML.load(meta)
-    article.date        = Time.parse article.date.to_s
     article.content     = content
-    article.misc        = misc
+    article.date        = Time.parse article.date.to_s
     article.slug        = File.basename(filename, '.md')
     article.author_slug = article.author.downcase # TODO: #underscore
     article.tags        = TagList(article.tags)
     article
-  end
-
-  def self.split_misc_from_content(content)
-    content.split /\n\n\/\/(NOTES|NOTE|OUTLINE|MISC)\/\/\n\n/, 2
   end
 end
